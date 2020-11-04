7ca41674747e6f36c89e231a88b3e04ecc922063
Joao Dorea <joao.dorea@contabilizei.com.br>
2020-11-04 09:57:31 -0300

    refactor: separating mock from test

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,16 +1,8 @@
-import { HttpPostClient } from './protocols/http/http-post-client'
+import { HttpPostClientSpy } from '../../../data/test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with corrent URL ', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-
-      async post (url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
     const url = 'any_url'
     const httpPostClientSpy = new HttpPostClientSpy()
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
