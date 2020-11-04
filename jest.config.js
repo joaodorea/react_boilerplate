cfccdab219e763d22a25b3dfd6dd577b20b6bc53
Joao Dorea <joao.dorea@contabilizei.com.br>
2020-11-04 09:57:05 -0300

    chore: fixing testEnvironment prop name

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: [
-        '<rootDir>/src/**/*.{ts,tsx}'
-    ],
-    coverageDirectory: 'coerage',
-    testEnviroment: 'node',
-    transform: {
-        '.+\\.ts$': 'ts-jest'
-    }
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coerage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 }
